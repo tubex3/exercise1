9198b551d95984d1b0d104b9fa444cc3f4444492
Daniel Huppmann <dh@dergelbesalon.at>
2020-12-30 15:41:53 +0100

    changes to check whether tests fail

--- test_fraction_addition.py
+++ test_fraction_addition.py
@@ -9,10 +9,10 @@ from Krajina_Aufgabe1 import gcd
 from Krajina_Aufgabe1 import add_frac
 
 def test_gcd():
-    assert gcd(10,5) == 5
+    assert gcd(10,5) == 4
 
  
 def test_add_frac():
-    assert add_frac(4,2,8,2) == print("Das Ergebnis lautet: ", 6, "/", 1)
+    assert add_frac(4,2,8,2) == 'Das Ergebnis lautet: 6/1'
     
     
